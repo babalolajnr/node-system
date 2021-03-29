a0bac5689ddde7562603b8eccef40ff3a7c337b1
babalolajnr <babalolajnr@gmail.com>
2021-03-29 11:03:04 +0100

    added console messages

--- src/App.java
+++ src/App.java
@@ -6,6 +6,7 @@ public class App {
         String nodeID;
         int nodePortNumber;
         int maxJobs;
+        String message;
 
         Scanner scan = new Scanner(System.in);
 
@@ -21,9 +22,21 @@ public class App {
         System.out.println("Enter Maximum Jobs");
         maxJobs = scan.nextInt();
 
+        System.out.println("Creating a new node ...");
+
         NodeManager nodeManager = new NodeManager();
         Node node = new Node(nodeIPAddress, nodeID, nodePortNumber, maxJobs);
         nodeManager.registerNewNode(node);
+
+        System.out.println("Node created!");
+
+        System.out.println("Send message");
+        message = scan.nextLine();
+
+        System.out.println("Sending message ...");
+        node.sendMessage(message);
+
+        System.out.println("Message sent ...");
 
         scan.close();
     }
